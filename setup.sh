2595cabd236a510c03bfce2f9c646b3b2871ad2e
Chris <clivingston45@gmail.com>
2019-04-30 14:10:29 -0700

    Iterate on install script, link subl symlink to the dot app

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ function banner() {
   printf "\033[0;90m[\033[0;94m${1}\033[0;90m]\033[0m\n"
 }
 
-banner "Setting up Chris' dev tools...let us rock"
+banner "Setting up Chris' dev tools...let us rock!"
 
 # Install homebrew
-banner "Installing Homebrew..."
+banner "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 
-banner "Installing brew packages..."
+banner "Installing brew packages"
 brew install $(cat brew_pkgs)
 
 # Install plugin manager for nvim
@@ -20,25 +20,32 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 mkdir -p ~/.config/nvim/
 cp ./init.vim ~/.config/nvim/  # Setup NVIM config
-cp ./.bash_profile ~/  # Set my bash profile
 
 # Install iTerm
-banner "Installing iTerm..."
+banner "Installing iTerm"
 brew cask install iterm2
 cp ./com.googlecode.iterm2.plist ~/
+
+# ST3
+banner "Installing ST3"
+brew cask install sublime-text
 
 # Get ZSH, install it
 banner "Installing ZSH"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ./.zshrc ~/
-cp ./minima.zsh-theme ~/.oh-my-zsh/themes
 
 banner "Installing shiftit"
 brew cask install shiftit
 
+banner "Installing config files"
 curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-cp ./git-prompt.sh ~/
-cp ./.gitconfig ~/
+cp ./git-prompt.sh ~/.git-prompt.sh
+cp ./.gitconfig ~/.gitconfig
+cp ./.bash_profile ~/.bash_profile
+cp ./.zshrc ~/.zshrc
+cp ./minima.zsh-theme ~/.oh-my-zsh/themes
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
 
 # Use ZSH.
 chsh -s /bin/zsh
